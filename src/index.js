c613792b0ecc03ea654ee00b0e556e77fbb1cbfd
pmita <panos.mita2@outlook.com>
2021-12-08 23:24:38 +0000

    FEAT/Authentication context introduced

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/App.scss';
+// CONTEXT
+import { AuthContextProvider } from './context/AuthContext'
 
 ReactDOM.render(
-    <App />,
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>,
   document.getElementById('root')
 );
 
